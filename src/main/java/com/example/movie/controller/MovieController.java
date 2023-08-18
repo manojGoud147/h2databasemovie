552971d35f6fc66fc2f79049546f75851b08880d
manojgoud147 <manogoud147@gmail.com>
2023-08-18 08:08:30 +0000

    error

---
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -0,0 +1,46 @@
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ * 
+ */
+
+// Write your code here
+package com.example.movie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+import com.example.movie.model.Movie;
+import com.example.movie.service.MovieJpaService;
+
+@RestController
+public class MovieController{
+
+    @Autowired
+    MovieJpaService movieJpaService;
+
+    @GetMapping("/movies")
+    public ArrayList<Movie> getMovies(){
+        return movieJpaService.getMovies();
+    }
+    @GetMapping("/movies/{movieId}")
+    public Movie getMovieById(@PathVariable("movieId") int movieId){
+        return movieJpaService.getMovieById(movieId);
+    }
+    @PostMapping("/movies")
+    public Movie addMovie(@RequestBody Movie movie){
+        return movieJpaService.addMovie(movie);
+    }
+    @PutMapping("/movies/{movieId}")
+    public Movie updateMovie(@PathVariable("movieId") int movieId, @RequestBody Movie movie){
+        return movieJpaService.updateMovie(movieId, movie);
+    }
+    @DeleteMapping("/movies/{movieId}")
+    public void deleteMovie(@PathVariable("movieId") int movieId){
+         movieJpaService.deleteMovie(movieId);
+    }
+}
